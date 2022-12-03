51c5b894400c056973901b97586459d9974e24ac
sbullocks <sbullocks@gmail.com>
2022-12-03 17:54:33 -0500

    updated about me section.

---
+++ 00-react-portfolio-app/src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="name-title">Stephen Bullocks</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://t4.ftcdn.net/jpg/01/71/94/39/360_F_171943908_K9zdR8EcRJFxGFC1sIzvKRvaoSWRBTmL.jpg" alt="picture of me..placeholder image currently" />
+                <p>
+                Currently attending bootcamp at UCF for Full Stack Web Development! 
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
